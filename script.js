47d18c3d1cd9bc33c564924fbec4a6cfe4caa07b
Rohit kanojiya <84227397+1Rohit-kanojiya@users.noreply.github.com>
2021-12-23 21:03:53 +0530

    My Ice-cream shop

---
+++ script.js
@@ -0,0 +1,14 @@
+
+let menu = document.querySelector('.bar');
+let navbar = document.querySelector('.nav');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+};
